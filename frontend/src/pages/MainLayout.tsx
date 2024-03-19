e7e7f1bad5cb5bfdd3fe225bd3e371f3eb81c012
Mirko Mälicke <mirko@hydrocode.de>
2024-03-19 09:05:34 +0100

    add version info

--- frontend/src/pages/MainLayout.tsx
+++ frontend/src/pages/MainLayout.tsx
@@ -1,7 +1,8 @@
-import { Layout, Menu } from "antd"
+import { Flex, Layout, Menu, theme } from "antd"
 import { SettingOutlined, UploadOutlined } from "@ant-design/icons"
 import { Outlet, Link } from "react-router-dom"
 
+import { version } from "../../package.json"
 
 
 // get some layout props
@@ -9,10 +10,15 @@ const {  Header, Sider } = Layout
 
 
 const MainLayout: React.FC = () => {
+    const { token: {colorTextLightSolid } } = theme.useToken()
     return <>
         <Layout style={{minHeight: '100vh', maxHeight: '100vh'}}>
             <Sider collapsible>
-            <Header>
+            <Header style={{color: colorTextLightSolid}}>
+                <Flex vertical>
+                
+                <div>Version: {version} </div>
+                </Flex>
 
             </Header>
             <Menu
